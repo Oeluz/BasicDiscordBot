470ba6a58bd8097d50e8fa312b797517a717a217
Zhencheng Chen <42793066+Oeluz@users.noreply.github.com>
2020-12-17 20:58:59 -0500

    Logged in the bot. Can't recognize messages.

---
+++ BasicBot/BasicBot/Modules/Commands.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+using Discord;
+using Discord.Commands;
+
+namespace BasicBot.Modules
+{
+    [NamedArgumentType]
+    class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task Ping()
+        {
+            await ReplyAsync("Pong");
+        }
+    }
+}
---
+++ BasicBot/BasicBot/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Reflection;
+
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BasicBot
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient client;
+        private CommandService commands;
+        private IServiceProvider services;
+
+        public async Task RunBotAsync()
+        {
+            client = new DiscordSocketClient();
+            commands = new CommandService();
+            services = new ServiceCollection()
+                .AddSingleton(client)
+                .AddSingleton(commands)
+                .BuildServiceProvider();
+
+            string token = "Nzg5Mjk5MTEyNjM2ODQyMDI0.X9wCFA.ddO8VkV5CFamOfFKh0iiXkrFkNg";
+
+            client.Log += Client_Log;
+
+            await RegisterCommandsAsync();
+
+            await client.LoginAsync(TokenType.Bot, token);
+
+            await client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Client_Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsAsync()
+        {
+            client.MessageReceived += Client_MessageReceived;
+            await commands.AddModulesAsync(Assembly.GetEntryAssembly(), services);
+        }
+
+        private async Task Client_MessageReceived(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            var context = new SocketCommandContext(client, message);
+
+            //ignore the message if the sender is a bot
+            if (message.Author.IsBot) return;
+
+            int argPos = 0;
+            if (message.HasStringPrefix("!", ref argPos))
+            {
+                var result = await commands.ExecuteAsync(context, argPos, services);
+                Console.WriteLine(message.Content);
+
+                if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+            }
+        }
+    }
+}
